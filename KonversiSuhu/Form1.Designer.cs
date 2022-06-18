61a85ebeb6b19915488d2016e2f5caf922f2f82e
paley777 <valleryan1212@gmail.com>
2022-06-18 11:48:06 +0700

    Add project files.

---
+++ KonversiSuhu/Form1.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace KonversiSuhu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textCelcius = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textReamur = new System.Windows.Forms.TextBox();
+            this.textFahrenheit = new System.Windows.Forms.TextBox();
+            this.fromcel = new System.Windows.Forms.Button();
+            this.fromream = new System.Windows.Forms.Button();
+            this.fromfah = new System.Windows.Forms.Button();
+            this.reset = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textKelvin = new System.Windows.Forms.TextBox();
+            this.fromkel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(197, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(389, 51);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "KONVERSI SUHU";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(60, 150);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 33);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Celcius";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(60, 251);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 33);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Reamur";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(60, 355);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(134, 33);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Fahrenheit";
+            // 
+            // textCelcius
+            // 
+            this.textCelcius.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textCelcius.Location = new System.Drawing.Point(241, 150);
+            this.textCelcius.Name = "textCelcius";
+            this.textCelcius.Size = new System.Drawing.Size(245, 35);
+            this.textCelcius.TabIndex = 4;
+            this.textCelcius.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(200, 150);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(23, 33);
+            this.label5.TabIndex = 5;
+            this.label5.Text = ":";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(200, 251);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(23, 33);
+            this.label6.TabIndex = 6;
+            this.label6.Text = ":";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(200, 355);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(23, 33);
+            this.label7.TabIndex = 7;
+            this.label7.Text = ":";
+            // 
+            // textReamur
+            // 
+            this.textReamur.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textReamur.Location = new System.Drawing.Point(241, 251);
+            this.textReamur.Name = "textReamur";
+            this.textReamur.Size = new System.Drawing.Size(245, 35);
+            this.textReamur.TabIndex = 8;
+            this.textReamur.Text = "0";
+            // 
+            // textFahrenheit
+            // 
+            this.textFahrenheit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textFahrenheit.Location = new System.Drawing.Point(241, 353);
+            this.textFahrenheit.Name = "textFahrenheit";
+            this.textFahrenheit.Size = new System.Drawing.Size(245, 35);
+            this.textFahrenheit.TabIndex = 9;
+            this.textFahrenheit.Text = "0";
+            // 
+            // fromcel
+            // 
+            this.fromcel.Location = new System.Drawing.Point(543, 146);
+            this.fromcel.Name = "fromcel";
+            this.fromcel.Size = new System.Drawing.Size(175, 49);
+            this.fromcel.TabIndex = 10;
+            this.fromcel.Text = "Dari Celcius";
+            this.fromcel.UseVisualStyleBackColor = true;
+            this.fromcel.Click += new System.EventHandler(this.fromcel_Click);
+            // 
+            // fromream
+            // 
+            this.fromream.Location = new System.Drawing.Point(543, 247);
+            this.fromream.Name = "fromream";
+            this.fromream.Size = new System.Drawing.Size(175, 49);
+            this.fromream.TabIndex = 11;
+            this.fromream.Text = "Dari Reamur";
+            this.fromream.UseVisualStyleBackColor = true;
+            this.fromream.Click += new System.EventHandler(this.fromream_Click);
+            // 
+            // fromfah
+            // 
+            this.fromfah.Location = new System.Drawing.Point(543, 349);
+            this.fromfah.Name = "fromfah";
+            this.fromfah.Size = new System.Drawing.Size(175, 49);
+            this.fromfah.TabIndex = 12;
+            this.fromfah.Text = "Dari Fahrenheit";
+            this.fromfah.UseVisualStyleBackColor = true;
+            this.fromfah.Click += new System.EventHandler(this.fromfah_Click);
+            // 
+            // reset
+            // 
+            this.reset.Location = new System.Drawing.Point(270, 524);
+            this.reset.Name = "reset";
+            this.reset.Size = new System.Drawing.Size(175, 49);
+            this.reset.TabIndex = 13;
+            this.reset.Text = "Reset";
+            this.reset.UseVisualStyleBackColor = true;
+            this.reset.Click += new System.EventHandler(this.reset_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(60, 445);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(88, 33);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Kelvin";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(200, 445);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(23, 33);
+            this.label9.TabIndex = 15;
+            this.label9.Text = ":";
+            // 
+            // textKelvin
+            // 
+            this.textKelvin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textKelvin.Location = new System.Drawing.Point(241, 445);
+            this.textKelvin.Name = "textKelvin";
+            this.textKelvin.Size = new System.Drawing.Size(245, 35);
+            this.textKelvin.TabIndex = 16;
+            this.textKelvin.Text = "0";
+            // 
+            // fromkel
+            // 
+            this.fromkel.Location = new System.Drawing.Point(543, 441);
+            this.fromkel.Name = "fromkel";
+            this.fromkel.Size = new System.Drawing.Size(175, 49);
+            this.fromkel.TabIndex = 17;
+            this.fromkel.Text = "Dari Kelvin";
+            this.fromkel.UseVisualStyleBackColor = true;
+            this.fromkel.Click += new System.EventHandler(this.fromkel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(825, 627);
+            this.Controls.Add(this.fromkel);
+            this.Controls.Add(this.textKelvin);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.reset);
+            this.Controls.Add(this.fromfah);
+            this.Controls.Add(this.fromream);
+            this.Controls.Add(this.fromcel);
+            this.Controls.Add(this.textFahrenheit);
+            this.Controls.Add(this.textReamur);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textCelcius);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Aplikasi Konversi Suhu";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textCelcius;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textReamur;
+        private System.Windows.Forms.TextBox textFahrenheit;
+        private System.Windows.Forms.Button fromcel;
+        private System.Windows.Forms.Button fromream;
+        private System.Windows.Forms.Button fromfah;
+        private System.Windows.Forms.Button reset;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textKelvin;
+        private System.Windows.Forms.Button fromkel;
+    }
+}
+
---
+++ KonversiSuhu/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KonversiSuhu
+{
+    public partial class Form1 : Form
+    {
+        Double Cel, Fah, Rea, Kel;
+
+        private void fromfah_Click(object sender, EventArgs e)
+        {
+            Fah = Convert.ToDouble(textFahrenheit.Text);
+            Fah = Fah;
+            Cel = ((Fah - 32) * 5) / 9; // To Celcius
+            Rea = ((Fah - 32) * 4) / 9; // To Reamur
+            Kel = ((Fah - 32) * 5) / 9 + 273; // To Kelvin
+            textCelcius.Text = Convert.ToString(Cel);
+            textReamur.Text = Convert.ToString(Rea);
+            textKelvin.Text = Convert.ToString(Kel);
+        }
+
+        private void fromkel_Click(object sender, EventArgs e)
+        {
+            Kel = Convert.ToDouble(textKelvin.Text);
+            Kel = Kel;
+            Cel = Kel - 273; // To Celcius
+            Rea = ((Kel - 273) * 4) / 5; // To Reamur
+            Fah = (((Kel - 273) * 9) / 5) + 32; // To Fahrenheit
+            textCelcius.Text = Convert.ToString(Cel);
+            textFahrenheit.Text = Convert.ToString(Fah);
+            textReamur.Text = Convert.ToString(Rea);
+        }
+
+        private void reset_Click(object sender, EventArgs e)
+        {
+            textCelcius.Text = "0";
+            textCelcius.Text = Convert.ToString(0);
+            textReamur.Text = "0";
+            textReamur.Text = Convert.ToString(0);
+            textFahrenheit.Text = "0";
+            textFahrenheit.Text = Convert.ToString(0);
+            textKelvin.Text = "0";
+            textKelvin.Text = Convert.ToString(0);
+        }
+
+        private void fromream_Click(object sender, EventArgs e)
+        {
+            Rea = Convert.ToDouble(textReamur.Text);
+            Rea = Rea;
+            Cel = (Rea * 5) / 4; // To Celcius
+            Fah = ((Rea * 9) / 4) + 32; // To Fahrenheit
+            Kel = ((Rea * 5) / 4) + 273; // To Kelvin
+            textCelcius.Text = Convert.ToString(Cel);
+            textFahrenheit.Text = Convert.ToString(Fah);
+            textKelvin.Text = Convert.ToString(Kel);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void fromcel_Click(object sender, EventArgs e)
+        {
+            Cel = Convert.ToDouble(textCelcius.Text);
+            Cel = Cel;
+            Rea = (Cel * 4) / 5; // to Reamur
+            Fah = (Cel * 9 / 5) + 32; // to Fahrenheit
+            Kel = Cel + 273; // To Kelvin
+            textReamur.Text = Convert.ToString(Rea);
+            textFahrenheit.Text = Convert.ToString(Fah);
+            textKelvin.Text = Convert.ToString(Kel);
+        }
+
+    }
+}
